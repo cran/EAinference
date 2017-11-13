0396aefef4740174582e83f5ea20021aa573ffc9
Seunghyun Min <seunghyun@ucla.edu>
2017-11-13 09:14:13 +0000

    version 0.2.2

--- R/MHInference.R
+++ R/MHInference.R
@@ -38,7 +38,7 @@
 #' for each active coefficient.}
 #' @examples
 #' set.seed(123)
-#' n <- 5
+#' n <- 6
 #' p <- 10
 #' X <- matrix(rnorm(n*p),n)
 #' Y <- X %*% rep(1,p) + rnorm(n)
@@ -50,6 +50,10 @@
 #' nChain = 3, niterPerChain = 20, method = "coeff", parallel = parallel)
 #' postInference.MHLS(X = X, Y = Y, lbd = lbd, sig2.hat = 1, alpha = .05,
 #' nChain = 3, niterPerChain = 20, method = "coeff", parallel = parallel, returnSamples = TRUE)
+#' postInference.MHLS(X = X, Y = Y, lbd = lbd, sig2.hat = 1, alpha = .05,
+#' nChain = 3, niterPerChain = 20, method = "mu", parallel = parallel)
+#' postInference.MHLS(X = X, Y = Y, lbd = lbd, sig2.hat = 1, alpha = .05,
+#' nChain = 3, niterPerChain = 20, method = "mu", parallel = parallel, returnSamples = TRUE)
 #' @export
 postInference.MHLS <- function(X, Y, lbd, weights = rep(1, ncol(X)),
   tau = rep(1, ncol(X)), sig2.hat, alpha = .05, nChain = 10, method,
@@ -68,7 +72,7 @@ postInference.MHLS <- function(X, Y, lbd, weights = rep(1, ncol(X)),
   if (length(A)==0) {
     stop("Given lbd, active set is empty.")
   }
-  
+
   Y <- matrix(Y, , 1)
   X <- as.matrix(X)
   n <- nrow(X)
@@ -130,7 +134,7 @@ postInference.MHLS <- function(X, Y, lbd, weights = rep(1, ncol(X)),
                                   sigma.hat = sqrt(sig2.hat))
   } else {
     Plugin.seq <- PluginMu.MHLS(X = X, Y = Y, lbd = lbd,
-      ratioSeq = seq(0,2,by=0.01), alpha = 0.05, nChain = nChain, niter = 100,
+      ratioSeq = seq(0,1,by=0.01), alpha = 0.05, nChain = nChain, niter = 100,
       parallel = parallel, ncores = ncores)
   }
 
